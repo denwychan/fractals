6b53e03b2d2d55ee2fbdc0d5af321059b545e433
Den Chan <98968275+denwychan@users.noreply.github.com>
2022-09-06 16:25:03 +0100

    Flood fill function completed and being tested

--- src/fractals.cpp
+++ src/fractals.cpp
@@ -3,12 +3,15 @@
 #include "fractals.h"
 #include "error.h"
 #include "gwindow.h"
-#include <cmath>;
+#include <cmath>
 using namespace std;
+// Function prototype
+int floodFillHelper(
+        GWindow& window, int x, int y, int color, int backgroundColor, int pixelsChanged);
 
 void drawSierpinskiTriangle(GWindow& window, double x, double y, double size, int order) {
     if (order == 0) { // Base case: draw nothing if the order is 0
-        cout << endl;
+        cout << "I'm done drawing" << endl;
     } else if (x < 0 || y < 0 || size < 0 || order < 0) { // Base case: throw error if any parameter
         // is a negative number
         error("x, y, size and order must be a positive number");
@@ -25,7 +28,35 @@ void drawSierpinskiTriangle(GWindow& window, double x, double y, double size, in
 }
 
 int floodFill(GWindow& window, int x, int y, int color) {
-    // TODO: write this function
-
+    int backgroundColor = window.getPixel(x, y);
+    int pixelsChanged  = 0;
+    floodFillHelper(window, x, y, color, backgroundColor, pixelsChanged);
     return 0;   // this is only here so it will compile
+}
+
+int floodFillHelper(
+        GWindow& window, int x, int y, int color, int backgroundColor, int pixelsChanged) {
+    // Base case: return 0 if the flood fill colour is the same as the background colour
+    if (color == backgroundColor) {
+        return 0;
+    } else if (x < 0 || y < 0 || !window.inCanvasBounds(x, y)) {
+    // Base case: throw exception if x or y are negative, or if the pixel is out of bound
+        error("x and y must be a positive number and be in bounds of canvas");
+    } else if (window.getPixel(x, y) != backgroundColor) {
+    // Base case: stop exploring if the background colour changes
+        cout << "Number of pixels change" << pixelsChanged << endl;
+        return pixelsChanged;
+    } else {
+        // Colour the pixel
+        window.setPixel(x, y, color);
+        pixelsChanged++;
+        // Look left
+        floodFillHelper(window, x - 1, y, color, backgroundColor, pixelsChanged);
+        // Look right
+        floodFillHelper(window, x + 1, y, color, backgroundColor, pixelsChanged);
+        // Look up
+        floodFillHelper(window, x, y - 1, color, backgroundColor, pixelsChanged);
+        // Look down
+        floodFillHelper(window, x, y + 1, color, backgroundColor, pixelsChanged);
+    }
 }
