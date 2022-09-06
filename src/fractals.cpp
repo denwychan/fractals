1f857439771e5391ef75e7ae80ec817b3ebd87e5
Den Chan <98968275+denwychan@users.noreply.github.com>
2022-09-06 12:54:49 +0100

    Completed Sierpinski Triangle part of assignment

--- src/fractals.cpp
+++ src/fractals.cpp
@@ -6,24 +6,7 @@
 #include <cmath>;
 using namespace std;
 
-/*
- * Order 1 - 1 triangle pow(3, order - 1)
- * Line 1: drawLine(x, y, x + size, y)
- * Line 2: drawLine(x + size, y, x/2, (sqrt(3)/2) * size)
- * Line 3: drawLine(x/2, (sqrt(3)/2) * size, x, y)
- *
- * Order 2 - 3 triangles
- * Line 1: drawLine(x, y, x + (size/ 2), y)
- * Line 2: drawLine(x + (size/2), y, x/2, (sqrt(3)/2) * (size/2))
- * Line 3: drawLine(x/2, (sqrt(3)/2) * (size/2), x, y)
- *
- * Order 3 - 9 triangles, order 4
- *
- */
-
 void drawSierpinskiTriangle(GWindow& window, double x, double y, double size, int order) {
-    double triangles = pow(3, order - 1);
-    double triangleSize = size/ triangles;
     if (order == 0) { // Base case: draw nothing if the order is 0
         cout << endl;
     } else if (x < 0 || y < 0 || size < 0 || order < 0) { // Base case: throw error if any parameter
@@ -31,12 +14,13 @@ void drawSierpinskiTriangle(GWindow& window, double x, double y, double size, in
         error("x, y, size and order must be a positive number");
     } else if (order == 1){ // Base case: draw 1 triangle if the order is 1
         // Draw each line in order to form triangle
-        window.drawLine(x, y, x + triangleSize, y);
-        window.drawLine(x + triangleSize, y, triangleSize/2, (sqrt(3)/2) * triangleSize);
-        window.drawLine(triangleSize/2, (sqrt(3)/2) * triangleSize, x, y);
-    } else { // Recursive case
-        drawSierpinskiTriangle(window, x + triangleSize, y, size, order - 1);
-        drawSierpinskiTriangle(window, triangleSize/2, (sqrt(3)/2) * triangleSize, size, order - 1);
+        window.drawLine(x, y, x + size, y);
+        window.drawLine(x + size, y, x + (size/2), y + (sqrt(3)/2) * size);
+        window.drawLine(x + (size/2), y + (sqrt(3)/2) * size, x, y);
+    } else { // Recursive case: draw the cluster of 3 triangles
+        drawSierpinskiTriangle(window, x, y, size/2, order - 1);
+        drawSierpinskiTriangle(window, x + size/2, y , size/2, order - 1);
+        drawSierpinskiTriangle(window, x + (size/4), y + (sqrt(3)/2) * size/2, size/2, order - 1);
     }
 }
 
